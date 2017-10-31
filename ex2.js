0d90329c26917b0a4fcd9ce1ad2837bee569c44c
TafLew97 <32627190+TafLew97@users.noreply.github.com>
2017-10-31 02:45:12 -0500

    Exercise2-Look up the word definition

---
+++ ex2.js
@@ -0,0 +1,14 @@
+"use strict";
+function SearchTerms()
+{
+    var xhttp_req = new XMLHttpRequest();
+    xhttp_req.onreadystatechange = function() 
+    {
+        if (this.readyState == 4 && this.status == 200) 
+        {
+            alert(this.responseText);
+        }
+    };
+    xhttp_req.open("GET", "request.php?q="+"definition", true);
+    xhttp_req.send();
+}
