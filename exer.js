a2cd9b10dca9c357931c70880f550dac6774ee51
TafLew97 <32627190+TafLew97@users.noreply.github.com>
2017-11-02 04:16:35 -0500

    Exercise3-Look up any word

--- exer.js
+++ exer.js
@@ -1,14 +1,15 @@
 "use strict";
-function defAlert()
+function search()
 {
-    var xhttp_req = new XMLHttpRequest();
-    xhttp_req.onreadystatechange = function() 
+    var query = document.getElementById("queryTxtBox").value.toLowerCase();
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() 
     {
         if (this.readyState == 4 && this.status == 200) 
         {
-            alert(this.responseText);
+            document.getElementById("result").innerHTML = this.responseText;
         }
     };
-    xhttp_req.open("GET", "request.php?q="+"definition", true);
-    xhttp_req.send();
+    xhttp.open("GET", "request.php?q="+ query, true);
+    xhttp.send();
 }
