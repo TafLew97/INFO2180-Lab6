6ecfd71a6aa08606f6533191012961d6bcf3c334
TafLew97 <32627190+TafLew97@users.noreply.github.com>
2017-11-02 22:15:37 -0500

    Exercise4- XML Data

--- exer.js
+++ exer.js
@@ -13,3 +13,21 @@ function search()
     xhttp.open("GET", "request.php?q="+ query, true);
     xhttp.send();
 }
+
+function searchall(def){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+        if(xhttp.readyState === 4 && xhttp.status === 200){
+            var result = "<ol>";
+            var xml = xhttp.responseXML;
+            var terms = xml.getElementsByTagName("definition");
+            for(var index = 0; index < terms.length; index++){
+                result+="<li><h3>"+terms[index].getAttribute("name")+"</h3><p>"+terms[index].childNodes[0].nodeValue+"</p><p> -"+terms[index].getAttribute("author")+"</p></li>";
+            }
+            result+="</ol>";
+             document.getElementById("result").innerHTML = result;
+        }
+    };
+    xhttp.open("GET","request.php?q=&all=true",true);
+    xhttp.send();
+}
--- request.php
+++ request.php
@@ -1,9 +1,8 @@
 <?php
-
 // accept a term (keyword)
 // respond with a value
-
 $query = $_GET['q'];
+$all = $_GET['all'];
 $definition = [
     "definition" => "A statement of the exact meaning of a word, especially in a dictionary.",
     "bar" => "A place that sells alcholic beverages",
@@ -13,6 +12,36 @@ $definition = [
     "javascript" => "A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.",
     "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
 ];
+$xmldata = '<?xml version="1.0" encoding="UTF-8"?>
+<entries>
+    <definition name = "definition" author = "John">
+       A statement of the exact meaning of a word, especially in a dictionary.
+    </definition>
+    <definition name = "javascript" author ="Ashley">
+        A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.
+    </definition>
+    <definition name = "php" author = "George">
+        A server-side scripting language, and a powerful tool for making dynamic and interactive websites
+    </definition>
+    <definition name = "ajax" author = "Khadejah">
+        Technique which involves the use of javascript and xml (or JSON).
+    </definition>
+    <definition name = "css" author = "Kimberly">
+        A style sheet language used for describing the presentation of a document written in a markup language.
+    </definition>
+    <definition name = "html" author = "Jason">
+        The standard markup language for creating web pages and web applications.
+    </definition>
+    <definition name = "bar" author = "Jodi">
+        A place that sells alcholic beverages.
+    </definition>
+</entries>';
+header('Content-Type: text/xml');
+$xmlOutput = new SimpleXMLElement($xmldata);
+if($all){
+    echo $xmlOutput->asXML();
+}else{
+    print "<h3>" . strtoupper($query) . "</h3>";
+    print "<p>" . $definition[$query] . "</p>";
+}
 
-print "<h3>" . strtoupper($query) . "</h3>";
-print "<p>" . $definition[$query] . "</p>";
